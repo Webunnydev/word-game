b74f2694d4bfa5aa52a084a8fed689c28507173d
Yamuna Raja <webbyperso@gmail.com>
2023-07-13 17:43:24 +0530

    imported word_dictionary JS file

---
+++ script/word_dictionary.js
@@ -0,0 +1,8 @@
+export var words = ["EAWRNS", "GULEDAIO","LBEEID"];
+export var answer = ["ANSWER", "DIALOGUE", "EDIBLE"]
+
+export var meaning = ["The response to a phone call, question, or letter.",
+"A conversation between two or more people.",
+"Something suitable to be eaten.", 
+
+]
--- script/word_scramble.js
+++ script/word_scramble.js
@@ -1,5 +1,14 @@
+import {
+    words,
+    answer,
+    meaning
+} from './word_dictionary.js';
+
+
 $(".scramble").hide();
 document.querySelector(".start").addEventListener("click", function(){
     $(".start").slideUp();
     $(".scramble").slideDown();
 })
+
+ 
