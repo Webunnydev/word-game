d2f81452550a56df8fc2c08c42728bc74113ca1d
Yamuna Raja <webbyperso@gmail.com>
2023-07-15 22:07:50 +0530

    Display

--- script/word_dictionary.js
+++ script/word_dictionary.js
@@ -1,7 +1,7 @@
 export var words = ["EAWRNS", "GULEDAIO","LBEEID"];
 export var answer = ["ANSWER", "DIALOGUE", "EDIBLE"]
 
-export var meaning = ["The response to a phone call, question, or letter.",
+export var meaning = ["Response to a phone call, question, or letter.",
 "A conversation between two or more people.",
 "Something suitable to be eaten.", 
 
--- script/word_scramble.js
+++ script/word_scramble.js
@@ -11,3 +11,15 @@ document.querySelector(".start").addEventListener("click", function(){
     $(".scramble").slideDown();
 })
  
+var randWord = Math.floor(Math.random()*words.length);
+
+function wordScramble(){
+    document.querySelector("#word").innerHTML = words[randWord];
+    document.querySelector("#hint").innerHTML = "<b>HINT: </b>" + meaning[randWord];
+
+    var value = document.querySelector("#value");
+    if (value === answer[randWord]){
+        alert("correct");
+    }
+}
+wordScramble();
