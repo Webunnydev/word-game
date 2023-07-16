fc3bb32d9be67691f6a2cf2bbfc95d6fa72231c5
Yamuna Raja <webbyperso@gmail.com>
2023-07-16 22:07:18 +0530

    input field

--- script/word_scramble.js
+++ script/word_scramble.js
@@ -9,7 +9,7 @@ $(".scramble").hide();
 document.querySelector(".start").addEventListener("click", function(){
     $(".start").slideUp();
     $(".scramble").slideDown();
-})
+});
  
 var randWord = Math.floor(Math.random()*words.length);
 
@@ -17,9 +17,23 @@ function wordScramble(){
     document.querySelector("#word").innerHTML = words[randWord];
     document.querySelector("#hint").innerHTML = "<b>HINT: </b>" + meaning[randWord];
 
-    var value = document.querySelector("#value");
-    if (value === answer[randWord]){
-        alert("correct");
-    }
+
 }
 wordScramble();
+
+
+document.querySelector("#btn1").addEventListener("click", function(){
+    var inputAnswer = document.getElementById("answer").value.toUpperCase();
+    if(inputAnswer===answer[randWord]){
+        $(".note").html("<span>Congragulations</span>, you got the answer.");
+    }
+    else if(inputAnswer===""){
+        $(".note").html("<span>No Answer</span>");
+        $("span").css("color", "red");
+    }
+    else{
+        $(".note").html("<span>Incorrect</span>, you got the wrong answer.");
+        $("span").css("color", "red");
+    }
+});
+
