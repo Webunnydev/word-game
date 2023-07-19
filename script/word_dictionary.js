9b11d685b4a2f9d935e9766b6cedb36aaeae1032
Yamuna Raja <webbyperso@gmail.com>
2023-07-20 04:44:11 +0530

    data

--- script/word_dictionary.js
+++ script/word_dictionary.js
@@ -1,8 +1,13 @@
-export var words = ["EAWRNS", "GULEDAIO","LBEEID"];
-export var answer = ["ANSWER", "DIALOGUE", "EDIBLE"]
-
+export var words = ["EAWRNS", "GULEDAIO","LBEEID","BNAESEC","EDMCACAI","ODANYRBU","EMERBCA","OGFNEIR","LNEKOWDEG","QLUYFAI"];
+export var answer = ["ANSWER", "DIALOGUE", "EDIBLE","ABSENCE","ACADEMIC","BOUNDARY","EMBRACE","FOREIGN","KNOWLEDGE","QUALIFY"];
 export var meaning = ["Response to a phone call, question, or letter.",
 "A conversation between two or more people.",
 "Something suitable to be eaten.", 
-
-]
+"Lack, Unavailability",
+"Scholastic",
+"Bounds, Confines",
+"Accept",
+"Overseas",
+"Understanding, Comprehension",
+"Be eligible"
+];
