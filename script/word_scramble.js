c5642e5f287d8ff6369bad6a2e4c5ba63d9d3a88
Yamuna Raja <webbyperso@gmail.com>
2023-07-19 20:35:29 +0530

    audio imported

--- script/word_scramble.js
+++ script/word_scramble.js
@@ -21,41 +21,48 @@ function wordScramble(){
 }
 wordScramble();
 
+var timeLimit=30;
+var timeRemaining=setInterval(timeOut, 1000);
+    
+function timeOut(){
+    if (timeLimit === -1) {
+        alert("Hello");
+        clearTimeout(timeRemaining);
+    }
+    else {
+        $("#time").html(timeLimit + "s");
+        if(timeLimit<=10){
+            $("#time").css("background-color", "red",);
+        }
+        else{
+            $("#time").html(timeLimit + "s");
+        }
+        var audio = new Audio("../audio/countdown.mp3");
+        audio.play();
+        timeLimit--;
+    }
+};
 
 document.querySelector("#btn1").addEventListener("click", function(){
     var inputAnswer = document.getElementById("answer").value.toUpperCase();
     if(inputAnswer===answer[randWord]){
-        $(".note").html("<span>Congragulations</span>, you got the answer.").hide().delay(200).fadeIn();
+        $(".note").html("<span>Congragulations</span>, you got the answer. Try again").hide().delay(200).fadeIn();
+        timeOut();
+ 
     }
     else if(inputAnswer===""){
         $(".note").html("<span>No Answer</span>, Enter your answer to check.").hide().delay(200).fadeIn();
         $("span").css("color", "red");
+     
     }
     else{
         $(".note").html("<span>Incorrect</span>, you got the wrong answer.").hide().delay(200).fadeIn();
         $("span").css("color", "red");
+        timeOut();
+ 
     }
 });
 
 
 
-var timeLimit=12;
-var timerId = setInterval(timeOut, 1000);
-    
-function timeOut() {
-    if (timeLimit === -1) {
-        clearTimeout(timerId);
-        $("#time").html("Time out");
-    }
-    else {
-        $("#time").html(timeLimit + "s");
-        if(timeLimit<=10){
-            $("#time").css("background-color", "red");
-    }
-    else{
-        $("#time").html(timeLimit + "s");
-    }
-    timeLimit--;
-}
 
-};
