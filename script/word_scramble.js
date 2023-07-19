c879069d444eae1eef27a55ffcc92d759bde8d95
Yamuna Raja <webbyperso@gmail.com>
2023-07-20 03:24:02 +0530

    next word button

--- script/word_scramble.js
+++ script/word_scramble.js
@@ -9,16 +9,20 @@ $(".scramble").hide();
 document.querySelector(".start").addEventListener("click", function(key){
     $(".start").slideUp();
     $(".scramble").slideDown();
-    var timeLimit=30;
-    setInterval (function (){
+    var timeLimit=12;
+    var timeRemaining = setInterval(timeOut, 1000);
 
+    function timeOut(){
         if (timeLimit === -1) {
-            alert("Hello");
+            $(".note").html("<span>Reached Time Limit</span>, Try next word.").hide().delay(200).fadeIn();
+            var audio = new Audio("../audio/countdown2.mp3");
+            audio.play();
+            clearInterval(timeRemaining);
         }
         else {
             $("#time").html(timeLimit + "s");
             if(timeLimit<=10){
-                $("#time").css("background-color", "red",);
+                $("#time").css("background-color", "red",) 
             }
             else{
                 $("#time").html(timeLimit + "s");
@@ -27,47 +31,57 @@ document.querySelector(".start").addEventListener("click", function(key){
             audio.play();
             timeLimit--;
         }
-     },1000)
+     };
+
+    var randWord = Math.floor(Math.random()*words.length);
+
+    function wordScramble(){
+        var randWord = Math.floor(Math.random()*words.length);
+        document.querySelector("#word").innerHTML = words[randWord];
+        document.querySelector("#hint").innerHTML = "<b>HINT: </b>" + meaning[randWord];
+
+    };
+
+    function clicked(){
+        var inputAnswer = document.getElementById("answer").value.toUpperCase();
+        if(inputAnswer===answer[randWord]){
+            $(".note").html("<span>Congragulations</span>, you got the answer.").hide().delay(200).fadeIn();
+            $("input,#btn1").hide();
+            clearInterval(timeRemaining);
+            var audio = new Audio("../audio/countdown2.mp3");
+            audio.play();
+        }
+        else if(inputAnswer===""){
+            $(".note").html("<span>No Answer</span>, Enter your answer to check.").hide().delay(200).fadeIn();
+            $("span").css("color", "red");
+            var audio = new Audio("../audio/countdown2.mp3");
+            audio.play();
+        }
+        else{
+            $(".note").html("<span>Incorrect</span>, you got the wrong answer. Try next word").hide().delay(200).fadeIn();
+            $("span").css("color", "red");
+            $("input,#btn1").hide();
+            clearInterval(timeRemaining);
+            var audio = new Audio("../audio/countdown2.mp3");
+            audio.play();
+        }
+    };
+
+    document.querySelector("#btn1").addEventListener("click", clicked);
+    $(document).keydown(function(event) {
+    if (event.keyCode == 13) {
+        clicked();
+    }
+    });
+
+    document.querySelector("#btn2").addEventListener("click", function(){
+        wordScramble();
+        $("input,#btn1").show();
+    });
+
 });
  
-var randWord = Math.floor(Math.random()*words.length);
 
-function wordScramble(){
-    document.querySelector("#word").innerHTML = words[randWord];
-    document.querySelector("#hint").innerHTML = "<b>HINT: </b>" + meaning[randWord];
-
-
-}
-wordScramble();
-
-
-function clicked(){
-    var inputAnswer = document.getElementById("answer").value.toUpperCase();
-    if(inputAnswer===answer[randWord]){
-        $(".note").html("<span>Congragulations</span>, you got the answer.").hide().delay(200).fadeIn();
- 
-    }
-    else if(inputAnswer===""){
-        $(".note").html("<span>No Answer</span>, Enter your answer to check.").hide().delay(200).fadeIn();
-        $("span").css("color", "red");
-     
-    }
-    else{
-        $(".note").html("<span>Incorrect</span>, you got the wrong answer. Try Again").hide().delay(200).fadeIn();
-        $("span").css("color", "red");
-
- 
-    }
-};
-
-
-
-document.querySelector("#btn1").addEventListener("click", clicked);
-  $(document).keydown(function(event) {
-   if (event.keyCode == 13) {
-    clicked();
-   }
-  });
 
 
 
